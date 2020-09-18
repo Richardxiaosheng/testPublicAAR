1b898fce5e6580867fb1132bcca5ea27da85e9e7
wangxin02@5eplay.com <wangxin02@5eplay.com>
2020-09-18 09:25:21 +0800

    test

---
+++ testUtils/library/src/main/java/com/fiveplay/library/MyTestUtils.java
@@ -0,0 +1,21 @@
+package com.fiveplay.library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * ================================================
+ * 创 建 人：wx
+ * 版    本：1.0
+ * 创建日期：2020/9/18
+ * 描    述：
+ * 修 改 人：
+ * 修改时间：2020/9/18
+ * ================================================
+ */
+public class MyTestUtils {
+
+    public static void test(Context context,String s){
+        Toast.makeText(context,s,Toast.LENGTH_SHORT).show();
+    }
+}
