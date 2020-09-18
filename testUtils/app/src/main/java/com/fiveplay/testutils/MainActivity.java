d573e64328d1a0b684075e25db11c237a25c29eb
wangxin02@5eplay.com <wangxin02@5eplay.com>
2020-09-18 12:21:45 +0800

    3

--- testUtils/app/src/main/java/com/fiveplay/testutils/MainActivity.java
+++ testUtils/app/src/main/java/com/fiveplay/testutils/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.fiveplay.library.MyTestUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MyTestUtils.test(this,"2eedwdewqwqe");
+//        MyTestUtils.test(this,"2eedwdewqwqe");
     }
 }
