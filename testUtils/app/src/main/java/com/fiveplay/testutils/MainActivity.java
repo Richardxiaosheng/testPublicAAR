b5a287d8e4e99f778438031bac29b65efb13d772
wangxin02@5eplay.com <wangxin02@5eplay.com>
2020-09-18 12:44:09 +0800

    w

--- testUtils/app/src/main/java/com/fiveplay/testutils/MainActivity.java
+++ testUtils/app/src/main/java/com/fiveplay/testutils/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        myte
 //        MyTestUtils.test(this,"2eedwdewqwqe");
     }
 }
